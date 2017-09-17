8040db6cd4083daadf0305ea85e2297d1024d5f4
Erik Fecko <jazdecf3@yahoo.com>
2017-09-17 18:49:20 +0200

    Lesson 1

---
+++ src/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP!</title>
+  </head>
+  <body>
+
+<?php
+
+// one line comment
+
+/*
+multiline comment
+describe your code
+*/
+
+
+// Variables, types
+$rowCount = 5; // integer
+$title = 'Winter is coming'; // string
+$hasTitle = true; // boolean
+$colors = ['red', 'blue', 'green']; // array of strings
+
+// displays structured information about one or more expressions
+// that includes its type and value
+var_dump($colors);
+
+?>
+  </body>
+</html>
